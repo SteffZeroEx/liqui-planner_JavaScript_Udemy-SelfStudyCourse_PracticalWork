0c84dee1bc635c6f6c3e10d50d23c698afdb3c9b
SteffZeroEx <163508274+SteffZeroEx@users.noreply.github.com>
2024-06-12 10:31:40 +0200

    # if & else-if eingefügt und den Code aufgeräumt

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,34 +1,55 @@
-'use strict';
-
-// Eingabedaten holen
-let titel_1 = prompt('Titel: (z.B. Einkaufen, Gehalt)');
-console.log(`Titel: ${titel_1}`);
-let typ_1 = prompt('Typ: (Einnahme oder Ausgabe)');
-console.log(`Typ: ${typ_1}`);
-let betrag_1 = parseInt(prompt('Betrag: (in Cent)'));
-console.log(betrag_1);
-// z.B. 2064,56€ -> 206456 Cent
-console.log(`Betrag: ${betrag_1} ct`);
-let datum_1 = prompt('Datum (jjjj-mm-tt):');
-console.log(`Datum: ${datum_1}`);
-
-// Eingabedaten 2 holen
-let titel_2 = prompt("Titel: (z.B. Einkaufen, Gehalt)");
-console.log(`Titel: ${titel_2}`);
-let typ_2 = prompt("Typ: (Einnahme oder Ausgabe)");
-console.log(`Typ: ${typ_2}`);
-let betrag_2 = parseInt(prompt("Betrag: (in Cent)")); // parseInt = Methode -> verwandelt einen String in einen Integer/Number
-console.log(betrag_2);
-console.log(`Betrag: ${betrag_2} ct`);
-let datum_2 = prompt("Datum: (jjjj-mm-tt");
-console.log(`Datum: ${datum_2}`);
-
+"use strict";
 
 // Gesambilanz
-let einnahmen;
-let ausgaben;
-let bilanz = betrag_1 + betrag_2;
-console.log(`Bilanz: ${bilanz}`);
+let einnahmen = 0;
+let ausgaben = 0;
+let bilanz = 0;
+
+
+// Eingabedaten holen
+let titel_1 = prompt("Titel: (z.B. Einkaufen, Gehalt)");
+let typ_1 = prompt("Typ: (Einnahme oder Ausgabe)");
+let betrag_1 = parseInt(prompt("Betrag: (in Cent)"));
+let datum_1 = prompt("Datum (jjjj-mm-tt):");
+console.log(`Titel: ${titel_1}
+    Typ: ${typ_1}
+    Betrag: ${betrag_1} in ct
+    Datum: ${datum_1}`);
+
+    if (typ_1 === "Einnahme") {
+       einnahmen = einnahmen + betrag_1;
+       bilanz = bilanz + betrag_1;
+    } else if (typ_1 == "Ausgabe") {
+           ausgaben = ausgaben + betrag_1;
+           bilanz = bilanz - betrag_1;
+        } else {
+            console.log(`Der Typ ${typ_1} ist nicht bekannt!`);
+        }
+    
+
+let titel_2 = prompt("Titel: (z.B. Einkaufen, Gehalt)");
+let typ_2 = prompt("Typ: (Einnahme oder Ausgabe)");
+let betrag_2 = parseInt(prompt("Betrag: (in Cent)")); // parseInt = Methode -> verwandelt einen String in einen Integer/Number
+let datum_2 = prompt("Datum: (jjjj-mm-tt");
+console.log(`Titel: ${titel_2}
+    Typ: ${typ_2}
+    Betrag: ${betrag_2} in ct
+    Datum: ${datum_2}`);
+
+    if (typ_2 === "Einnahme") {
+        einnahmen = einnahmen + betrag_2;
+        bilanz = bilanz + betrag_2;
+    } else if (typ_2 === "Ausgabe") {
+        ausgaben = ausgaben + betrag_2;
+        bilanz = bilanz - betrag_2;
+    } else {
+        console.log(`Der Typ ${typ_2} ist nicht bekannt!`);
+    }
+
+
 
 let positiv = bilanz >= 0;
-console.log(`Bilanz ist positiv: ${positiv}`);
+console.log(`Einnahmen: ${einnahmen} ct
+    Ausgabe: ${ausgaben} ct
+    Bilanz: ${bilanz} ct
+    Bilanz ist positiv: ${positiv}`);
