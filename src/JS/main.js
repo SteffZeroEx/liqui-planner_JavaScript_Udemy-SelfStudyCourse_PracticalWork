c69de6bd2674b8ac54d860173d5da3ffcbd0b492
SteffZeroEx <163508274+SteffZeroEx@users.noreply.github.com>
2024-06-12 08:44:15 +0200

    #

--- src/JS/main.js
+++ src/JS/main.js
@@ -5,7 +5,8 @@ let titel_1 = prompt('Titel: (z.B. Einkaufen, Gehalt)');
 console.log(`Titel: ${titel_1}`);
 let typ_1 = prompt('Typ: (Einnahme oder Ausgabe)');
 console.log(`Typ: ${typ_1}`);
-let betrag_1 = prompt('Betrag: (in Cent)');
+let betrag_1 = parseInt(prompt('Betrag: (in Cent)'));
+console.log(betrag_1);
 // z.B. 2064,56€ -> 206456 Cent
 console.log(`Betrag: ${betrag_1} ct`);
 let datum_1 = prompt('Datum (jjjj-mm-tt):');
@@ -16,7 +17,8 @@ let titel_2 = prompt("Titel: (z.B. Einkaufen, Gehalt)");
 console.log(`Titel: ${titel_2}`);
 let typ_2 = prompt("Typ: (Einnahme oder Ausgabe)");
 console.log(`Typ: ${typ_2}`);
-let betrag_2 = prompt("Betrag: (in Cent)");
+let betrag_2 = parseInt(prompt("Betrag: (in Cent)")); // parseInt = Methode -> verwandelt einen String in einen Integer/Number
+console.log(betrag_2);
 console.log(`Betrag: ${betrag_2} ct`);
 let datum_2 = prompt("Datum: (jjjj-mm-tt");
 console.log(`Datum: ${datum_2}`);
@@ -27,3 +29,6 @@ let einnahmen;
 let ausgaben;
 let bilanz = betrag_1 + betrag_2;
 console.log(`Bilanz: ${bilanz}`);
+
+let positiv = bilanz >= 0;
+console.log(`Bilanz ist positiv: ${positiv}`);
