3116772bfeefd0456c5f4ded653c03866ce25aa4
SteffZeroEx <163508274+SteffZeroEx@users.noreply.github.com>
2024-07-03 09:25:42 +0200

    # Die Methode formulardaten_holen() eingebaut

--- src/JS/eingabeformular.js
+++ src/JS/eingabeformular.js
@@ -1,10 +1,32 @@
 "use strict";
 
 const eingabeformular = {
+
+  formulardaten_holen(e){
+    let typ;
+
+    if (e.target.elements.ausgabe.checked === true){
+      typ = "ausgabe";
+    } else if (e.target.elements.einnahme.checked) {
+      typ = "einnahme";
+    }
+
+    return {
+      titel: e.target.elements.titel.value,
+      betrag: e.target.elements.betrag.value,
+      typ: typ,
+      datum: e.target.elements.datum.valueAsDate
+    }
+  },
+
+
   absenden_event_hinzufügen(eingabeformular) {
     eingabeformular.querySelector("#eingabeformular").addEventListener("submit", e => {
       e.preventDefault();
       // Formulardaten holen
+      console.log(e);
+      let formulardaten = this.formulardaten_holen(e);
+      console.log(formulardaten);
       // Formulardaten verarbeiten
       // Formulardaten validieren
       // Wenn die Formulardaten valide sind
