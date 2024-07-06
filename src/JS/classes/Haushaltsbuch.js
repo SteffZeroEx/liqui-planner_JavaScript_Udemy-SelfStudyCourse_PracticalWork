043ed9fb02e8a49d81897ac94beaa53907fa2f22
SteffZeroEx <163508274+SteffZeroEx@users.noreply.github.com>
2024-07-06 18:45:07 +0200

    # Speichermöglichkeiten JSON und localStorage eingebaut und angepasst

--- src/JS/classes/Haushaltsbuch.js
+++ src/JS/classes/Haushaltsbuch.js
@@ -7,14 +7,16 @@ class Haushaltsbuch {
     this._eingabeformular = new Eingabeformular();
     this._monatslistensammlung = new Monatslistensammlung();
     this._gesamtbilanz = new Gesamtbilanz();
-    (console.log(this));
+    console.log(this);
+    this._wiederherstellen();
   }
 
-  eintrag_hinzufuegen(formulardaten) {
-    let neuer_eintrag = new Eintrag(formulardaten.titel, formulardaten.betrag, formulardaten.typ, formulardaten.datum);
+  eintrag_hinzufuegen(eintragsdaten) {
+    let neuer_eintrag = new Eintrag(eintragsdaten.titel, eintragsdaten.betrag, eintragsdaten.typ, eintragsdaten.datum);
     this._eintraege.push(neuer_eintrag);
     this._monatslistensammlung.aktualisieren(this._eintraege);
     this._gesamtbilanz.aktualisieren(this._eintraege);
+    this._speichern();
   }
 
   eintrag_entfernen(timestamp) {
@@ -28,6 +30,25 @@ class Haushaltsbuch {
     this._eintraege.splice(start_index, 1);
     this._monatslistensammlung.aktualisieren(this._eintraege);
     this._gesamtbilanz.aktualisieren(this._eintraege);
+    this._speichern();
+  }
+
+  _speichern() {
+    localStorage.setItem("eintraege", JSON.stringify(this._eintraege));
+  }
+
+  _wiederherstellen() {
+    let gespeicherte_eintraege = localStorage.getItem("eintraege");
+    if (gespeicherte_eintraege !== null) {
+      JSON.parse(gespeicherte_eintraege).forEach(eintrag => {
+        this.eintrag_hinzufuegen({
+          titel: eintrag._titel,
+          betrag: eintrag._betrag,
+          typ: eintrag._typ,
+          datum: new Date(eintrag._datum)
+        });
+      });
+    }
   }
 
   start() {
